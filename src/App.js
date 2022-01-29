1c664271f63d8aab1ce20d40f22546b2f451fd86
Joaquin Alvarez <joaquinjoby@gmail.com>
2022-01-29 01:54:58 -0300

    final2

--- src/App.js
+++ src/App.js
@@ -14,35 +14,6 @@ const CONTRACT_ADDRESS = "0xbb5Ac57682f8E301aF6eF2698eDa1c345fB191A5";
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
 
-  const checkIfWalletIsConnected = async () => {
-    const { ethereum } = window;
-
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-      return;
-    } else {
-      console.log("We have the ethereum object", ethereum);
-    }
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-
-    let chainId = await ethereum.request({ method: "eth_chainId" });
-    console.log("Connected to chain " + chainId);
-
-    const rinkebyChainId = "0x4";
-    if (chainId !== rinkebyChainId) {
-      alert("You are not connected to the Rinkeby Test Network!");
-    }
-
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account:", account);
-      setCurrentAccount(account);
-      setupEventListener();
-    } else {
-      console.log("No authorized account found");
-    }
-  };
-
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -125,6 +96,35 @@ const App = () => {
     }
   };
   useEffect(() => {
+    const checkIfWalletIsConnected = async () => {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      console.log("Connected to chain " + chainId);
+
+      const rinkebyChainId = "0x4";
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby Test Network!");
+      }
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        setupEventListener();
+      } else {
+        console.log("No authorized account found");
+      }
+    };
+
     checkIfWalletIsConnected();
   }, []);
 
